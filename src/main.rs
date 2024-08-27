38193cac408dc5e7081942de4ec42fdaff4052cd
Prashant-dot1 <puriprashant14@gmail.com>
2024-08-27 08:31:38 +0530

    most of the scanning part

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,51 @@
 use std::env;
 use std::fs;
 use std::io::{self, Write};
+use std::path::PathBuf;
+use clap::command;
+use clap::Parser;
+use clap::Subcommand;
+use codecrafters_interpreter::*;
+use miette::Context;
+use miette::IntoDiagnostic;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return;
-    }
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    let command = &args[1];
-    let filename = &args[2];
+#[derive(Subcommand,Debug)]
+enum Commands {
+    /// does testing things
+    Tokenize {
+        filename: PathBuf,
+    },
+}
 
-    match command.as_str() {
-        "tokenize" => {
+
+fn main() -> miette::Result<()>{
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Tokenize { filename } => {
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
 
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-                String::new()
-            });
+            let file_contents = fs::read_to_string(&filename)
+                                                        .into_diagnostic()
+                                                        .wrap_err_with(|| format!("reading '{}' failed" , filename.display()))?;
 
-            // Uncomment this block to pass the first stage
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            let lexer = Lexer::new(&file_contents);
+
+            for token in lexer {
+                let token = token?;
+                println!("{token}");
             }
-        }
-        _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
+            println!("EOF  null");
         }
     }
+
+    Ok(())
 }
